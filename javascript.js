7e12504284a4907d7cd74e00f071a5d9cbf10a4e
Mads Hougesen <mads@mhouge.dk>
2020-02-18 13:13:27 +0100

    font size

--- javascript.js
+++ javascript.js
@@ -36,10 +36,10 @@ window.onscroll = function () {
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("navbar").style.backgroundColor = "white";
-        document.getElementById("logo").style.fontSize = "25px";
+        document.getElementById("logo").style.fontSize = "30px";
     } else {
-        document.getElementById("navbar").style.backgroundColor = "transparent";
 
-        document.getElementById("logo").style.fontSize = "35px";
+        document.getElementById("navbar").style.backgroundColor = "transparent";
+        document.getElementById("logo").style.fontSize = "40px";
     }
 }
