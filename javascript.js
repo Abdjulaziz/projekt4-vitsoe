70e9bf6dcacefdaa92267b757bca0310a483f38a
Mads Hougesen <mads@mhouge.dk>
2020-02-28 10:59:42 +0100

    fokus på scroll samt layout ændringer

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-// Navagations animation
-
 window.onscroll = function () {
     scrollFunction()
 };
@@ -20,12 +18,10 @@ function scrollFunction() {
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
