3b34d02d215272509d25b4d62806aa0412dec0c8
Mads Hougesen <mads@mhouge.dk>
2020-03-06 11:25:07 +0100

    Endelig version af hjemmeside før aflevering
    Tilføjelse af content på artiklerne. Optimering af billederne. Rettelser inden aflevering

--- javascript.js
+++ javascript.js
@@ -5,12 +5,14 @@ window.onscroll = function () {
 
 function scrollFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        document.getElementById("navbar").style.backgroundColor = "white";
+        document.getElementById("navbar").style.backgroundColor = "#f8f8f8";
         document.getElementById("logo").style.fontSize = "30px";
+
     } else {
 
         document.getElementById("navbar").style.backgroundColor = "transparent";
         document.getElementById("logo").style.fontSize = "40px";
+
     }
 }
 
